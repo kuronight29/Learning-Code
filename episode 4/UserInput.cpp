42120b47d5e9d3dcf24541da976413feb2d0d5ad
kuronight29 <nightkuro29@gmail.com>
2022-09-21 20:29:28 +0700

    User Input

---
+++ episode 4/UserInput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x, y;
+  int sum;
+  cout << "Số thứ nhất: "; // Khai báo biến
+  cin >> x; // Nhận thông tin nhập của người dùng từ bàn phím
+  cout << "Số thứ hai: "; // Khai báo biến
+  cin >> y; // Nhận thông tin nhập của người dùng từ bàn phím
+  sum = x + y; // tổng biến thứ nhất và biến thứ 2
+  cout << "tổng hai số: " << sum; // Gọi biến
+  return 0;
+}
